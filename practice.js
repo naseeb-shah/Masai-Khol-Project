cb7f582a5568a78c089c6a1279840afa7d2fbdd4
naseeb-shah <102802675+naseeb-shah@users.noreply.github.com>
2022-04-17 20:44:50 +0530

    all file updared

---
+++ practice.js
@@ -0,0 +1,10 @@
+sai={
+    add:{
+        villege:"leda khadar",
+        pincode:"135103"
+    },
+    cart:[],
+    about:"denashah"
+}
+console.log(sai.add.villege)
+console.log(sai.add.pincode)
